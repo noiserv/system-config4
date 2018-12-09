dfcc75e58170ca872c62da8a63ff9720fd869054
none <none>
2018-12-09 05:23:00 -0500

    tested RI a) and added testing INSERTs

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,4 @@
-﻿﻿
--- a) Um Coordenador só pode solicitar vídeos de câmaras colocadas num local cujo
+﻿-- a) Um Coordenador só pode solicitar vídeos de câmaras colocadas num local cujo
 -- accionamento de meios esteja a ser (ou tenha sido) auditado por ele próprio.
 
 CREATE OR REPLACE FUNCTION solicita_permit() RETURNS TRIGGER AS $body$
@@ -12,11 +11,12 @@ CREATE OR REPLACE FUNCTION solicita_permit() RETURNS TRIGGER AS $body$
               NATURAL JOIN vigia
               WHERE idCoordenador = new.idCoordenador
               AND   moradaLocal   = morada
-              AND   dataAuditoria <= NOW()
+              --AND   dataAuditoria <= NOW()
             );
     IF success = 0 THEN
         RAISE EXCEPTION 'Update of time';
     END IF;
+    RETURN new;
   END;
 $body$ LANGUAGE plpgsql;
 
@@ -24,15 +24,9 @@ DROP TRIGGER IF EXISTS solicita_permit on solicita;
 CREATE TRIGGER solicita_permit BEFORE INSERT ON solicita
   FOR EACH ROW EXECUTE PROCEDURE solicita_permit();
 
-INSERT INTO acciona VALUES ('30','EQETDSZEJF','45'); -- meio 30 acionado por processo 45
-INSERT INTO solicita VALUES ('30','2018-08-12 10:00:00','45','2018-08-12 10:00:00','2018-08-12 10:59:59');
-
--- select * from vigia where camNum = 30;
--- SELECT * FROM audita NATURAL JOIN eventoEmergencia WHERE idCoordenador = 30
-
-
--- SELECT count(*) FROM audita NATURAL JOIN eventoEmergencia
-   --           NATURAL JOIN vigia
-     --         WHERE idCoordenador = 30
-             -- AND   moradaLocal   = (select moradaLocal from vigia where camNum = 30)
-;
+-- code for testing a)
+INSERT INTO camara VALUES (140);
+INSERT INTO video VALUES ('140','2018-08-12 10:00:00','2018-08-12 10:59:59');
+INSERT INTO vigia VALUES ('Praça do Império, Frente Mosteiro dos Jerónimos', '140');
+INSERT INTO acciona VALUES ('31','JGIWYABTIA','45'); -- meio 30 acionado por processo 45
+INSERT INTO solicita VALUES ('31','2018-08-12 10:00:00','140','2018-08-12 10:00:00','2018-08-12 10:59:59');
