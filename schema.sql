2aca7b5a86087c1eb056bb9c50cbac5972ba4626
none <none>
2018-12-09 08:37:05 -0500

    FIX: triggers need to be mixed in the code

--- schema.sql
+++ schema.sql
@@ -52,7 +52,7 @@ CREATE TABLE vigia (
   moradaLocal VARCHAR(255) NOT NULL,
   camNum integer NOT NULL,
   PRIMARY KEY(moradaLocal,camNum),
-  FOREIGN KEY(camNum) REFERENCES camara(camNum)  ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(camNum) REFERENCES camara(camNum)  ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY(moradaLocal) REFERENCES zona(moradaLocal)  ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -117,15 +117,6 @@ CREATE TABLE transporta(
   FOREIGN KEY(numMeio,nomeEntidade) REFERENCES meioSocorro(numMeio, nomeEntidade)  ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE alocado(
-  numMeio NUMERIC(255) NOT NULL,
-  nomeEntidade VARCHAR(255) NOT NULL,
-  numProcessoSocorro NUMERIC(255) NOT NULL ,
-  numHoras NUMERIC(255) NOT NULL,
-  PRIMARY KEY(numMeio, nomeEntidade,numProcessoSocorro),
-  FOREIGN KEY(numProcessoSocorro) REFERENCES processoSocorro(numProcessoSocorro) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY(numMeio,nomeEntidade) REFERENCES meioApoio(numMeio, nomeEntidade)  ON DELETE CASCADE ON UPDATE CASCADE
-);
 
 CREATE TABLE acciona(
   numMeio NUMERIC(255) NOT NULL,
@@ -135,6 +126,44 @@ CREATE TABLE acciona(
   FOREIGN KEY(numProcessoSocorro) REFERENCES processoSocorro(numProcessoSocorro)  ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY(numMeio,nomeEntidade) REFERENCES meio(numMeio, nomeEntidade)  ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE alocado(
+  numMeio NUMERIC(255) NOT NULL,
+  nomeEntidade VARCHAR(255) NOT NULL,
+  numProcessoSocorro NUMERIC(255) NOT NULL ,
+  numHoras NUMERIC(255) NOT NULL,
+  PRIMARY KEY(numMeio, nomeEntidade,numProcessoSocorro),
+  FOREIGN KEY(numProcessoSocorro) REFERENCES processoSocorro(numProcessoSocorro) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(numMeio,nomeEntidade) REFERENCES meioApoio(numMeio, nomeEntidade)  ON DELETE CASCADE ON UPDATE CASCADE
+);
+-- b) Um Meio de Apoio só pode ser alocado a Processos de Socorro para os quais tenha
+-- sido accionado.
+
+-- uma maneira alterativa de fazer isto seria adicionar uma foreign key a acciona
+-- FOREIGN KEY(numMeio, nomeEntidade,numProcessoSocorro) REFERENCES alocado(numMeio, nomeEntidade,numProcessoSocorro)
+-- em vez das outras foreign keys
+-- FIXME perguntar aos profes se podemos alterar o modelo relacional para a restricao FIXME
+
+CREATE OR REPLACE FUNCTION accionado_before_alocado() RETURNS TRIGGER AS $body$
+    DECLARE success INT;
+    DECLARE morada VARCHAR(255);
+    BEGIN
+    success := (SELECT count(*) FROM acciona
+                WHERE new.numMeio = numMeio
+                AND new.nomeEntidade = nomeEntidade
+                AND new.numProcessoSocorro = numProcessoSocorro
+            );
+    IF success = 0 THEN
+        RAISE EXCEPTION 'restriction b) violated';
+    END IF;
+    RETURN new;
+  END;
+$body$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS accionado_before_alocado on alocado;
+CREATE TRIGGER accionado_before_alocado BEFORE INSERT ON alocado
+  FOR EACH ROW EXECUTE PROCEDURE accionado_before_alocado();
+-- end of trigger
 
 CREATE TABLE coordenador (
     idCoordenador NUMERIC(255) NOT NULL unique,
@@ -157,6 +186,7 @@ CREATE TABLE audita(
   CHECK (dataAuditoria <= NOW())
 );
 
+
 CREATE TABLE solicita(
     idCoordenador NUMERIC(255) NOT NULL ,
     dataHoraInicioVideo TIMESTAMP NOT NULL,
@@ -167,3 +197,32 @@ CREATE TABLE solicita(
     FOREIGN KEY(idCoordenador) REFERENCES coordenador(idCoordenador) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY(dataHoraInicioVideo,camNum) REFERENCES video(dataHoraInicio,camNum) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+-- a) Um Coordenador só pode solicitar vídeos de câmaras colocadas num local cujo
+-- accionamento de meios esteja a ser (ou tenha sido) auditado por ele próprio.
+
+CREATE OR REPLACE FUNCTION solicita_permit() RETURNS TRIGGER AS $body$
+    DECLARE success INT;
+    DECLARE morada VARCHAR(255);
+    BEGIN
+    morada  := (SELECT moradaLocal FROM vigia
+                  WHERE new.camNum = camNum);
+    success := (SELECT count(*) FROM audita NATURAL JOIN eventoEmergencia
+              NATURAL JOIN vigia
+              WHERE idCoordenador = new.idCoordenador
+              AND   moradaLocal   = morada
+              AND   dataAuditoria <= NOW()
+            );
+    IF success = 0 THEN
+        RAISE EXCEPTION 'restriction a) violated when inserting %', new ;
+    END IF;
+    RETURN new;
+  END;
+$body$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS solicita_permit on solicita;
+CREATE TRIGGER solicita_permit BEFORE INSERT ON solicita
+  FOR EACH ROW EXECUTE PROCEDURE solicita_permit();
+
+-- end of trigger
+
