219580470dc80a7fbd8d93bc750b738556288966
none <none>
2018-12-12 10:21:14 -0500

    sequential time generation

--- dataPopulate.sql
+++ dataPopulate.sql
@@ -1,18 +1,32 @@
-INSERT INTO d_evento(numTelefone,instanteChamada)
+﻿INSERT INTO d_evento(numTelefone,instanteChamada)
 SELECT numTelefone, instanteChamada FROM eventoEmergencia;
 
 INSERT INTO d_meio(numMeio,nomeEntidade,tipo)
 SELECT numMeio, nomeEntidade,'Apoio' FROM meioApoio;
+
 INSERT INTO d_meio(numMeio,nomeEntidade,tipo)
 SELECT numMeio, nomeEntidade,'Combate' FROM meioCombate;
+
 INSERT INTO d_meio(numMeio,nomeEntidade,tipo)
 SELECT numMeio, nomeEntidade,'Socorro' FROM meioSocorro;
 
-INSERT INTO d_tempo(dia,mes,ano)
-SELECT extract(day from instanteChamada) AS dia,extract(month from instanteChamada) as mes, extract(year from instanteChamada) as ano FROM eventoEmergencia;
 
-INSERT INTO factos(idEvento,idMeio,idTempo)
-SELECT e.idEvento,m.idMeio,t.idTempo
+/* insertion of the time range from the oldest instantechamada to the newest */
+INSERT INTO d_tempo(tempo_id,dia,mes,ano)
+select getTime_id(timerange),
+       extract(day FROM timerange),
+       extract(month FROM timerange),
+       extract(year FROM timerange)
+from generate_series((SELECT min(instanteChamada) -- min
+			FROM eventoEmergencia),
+		      (SELECT max(instanteChamada)
+			FROM eventoEmergencia), '1day') AS timerange;
+
+/* Creation of the Facts Table */
+/*
+INSERT INTO factos(idEvento,idMeio,tempo_id)
+SELECT e.idEvento,m.idMeio,t.tempo_id
 FROM d_evento e
 INNER JOIN d_meio m ON e.idEvento=m.idMeio
-INNER JOIN d_tempo t ON m.idMeio=t.idTempo --colocar data, usar produto das cartesianas??
+INNER JOIN d_tempo t ON m.idMeio=t.tempo_id --colocar data, usar produto das cartesianas??
+*/
--- dataWarehouse.sql
+++ dataWarehouse.sql
@@ -1,7 +1,38 @@
-DROP TABLE IF EXISTS factos;
+﻿DROP TABLE IF EXISTS factos;
 DROP TABLE IF EXISTS d_evento;
 DROP TABLE IF EXISTS d_meio;
 DROP TABLE IF EXISTS d_tempo;
+
+
+CREATE OR REPLACE FUNCTION getTime_id (tempo timestamp) RETURNS INTEGER AS $body$
+  DECLARE dia INTEGER := extract( day FROM (
+			SELECT min(instanteChamada)
+			FROM eventoEmergencia
+			));
+  DECLARE mes INTEGER := extract( month FROM (
+				SELECT min(instanteChamada)
+				FROM eventoEmergencia
+				));
+  DECLARE ano INTEGER := extract( year FROM (
+				SELECT min(instanteChamada)
+				FROM eventoEmergencia
+				));
+  BEGIN
+    return dia  + mes*100 + ano * 10000 ;
+  END;
+$body$ LANGUAGE plpgsql;
+
+/** Function that generates the time intervals from a given minimum to a given maximum from
+ *
+ **/
+CREATE OR REPLACE FUNCTION genTimeIntervals(min timestamp,
+                                            max timestamp) RETURNS TABLE (tempo timestamp) AS $body$
+  BEGIN
+	return QUERY SELECT current_date + i
+		FROM generate_series(min - current_date,
+			max - current_date ) i;
+  END;
+$body$ LANGUAGE plpgsql;
 
 CREATE TABLE d_evento(
   idEvento SERIAL UNIQUE,
@@ -19,21 +50,20 @@ CREATE TABLE d_meio(
 );
 
 CREATE TABLE d_tempo(
+  tempo_id INTEGER NOT NULL,
   dia INTEGER NOT NULL ,
   mes INTEGER NOT NULL,
   ano INTEGER NOT NULL,
-  PRIMARY KEY(dia,mes,ano)
+  PRIMARY KEY(tempo_id)
 );
 
 CREATE TABLE factos(
   idFacto INTEGER,
   idEvento INTEGER,
   idMeio INTEGER,
-  dia INTEGER NOT NULL ,
-  mes INTEGER NOT NULL,
-  ano INTEGER NOT NULL,
+  tempo_id INTEGER,
   PRIMARY KEY(idFacto),
   FOREIGN KEY(idEvento) REFERENCES d_evento(idEvento),
   FOREIGN KEY(idMeio) REFERENCES d_meio(idMeio),
-  FOREIGN KEY(dia,mes,ano) REFERENCES d_tempo(dia,mes,ano)
+  FOREIGN KEY(tempo_id) REFERENCES d_tempo(tempo_id)
 );
